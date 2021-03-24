030b6306330ac7fe0a3e8e2f90ac94f2bf8abb8c
Heather Ackerman <69919170+HeatherAckerman@users.noreply.github.com>
2021-03-24 12:47:57 -0500

    Set up server for passport and sequelize added to back and frontend folder structures

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+var express = require("express");
+var session = require("express-session");
+var passport = require("./config/passport");
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+var app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Sessions to keep track of users login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Requiring our routes
+require("./routes/routes.js")(app);
+require("./controllers/controller.js")(app);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -14,6 +30,9 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync({force: true}).then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
 });
